4acfc8bc06db472acf279d6c569db46917cb3e6a
Hugo A. Martínez Lagos <hmartinez.lagos@gmail.com>
2017-10-13 17:42:21 -0300

    Creación del proyecto

---
+++ demoIOS2/demoIOS2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoIOS2
+//
+//  Created by Hugo Martínez Lagos on 13-10-17.
+//  Copyright © 2017 SAWP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
